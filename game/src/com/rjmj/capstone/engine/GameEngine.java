8109cb5e2083e1a32ec1b888ede8d70c8f527caa
josh <shike25@gmail.com>
2020-07-15 17:00:29 -0400

    TODO update

---
+++ game/src/com/rjmj/capstone/engine/GameEngine.java
@@ -0,0 +1,46 @@
+package com.rjmj.capstone.engine;
+
+// Imports go here
+import java.util.Scanner.*;
+
+
+
+public class GameEngine {
+
+    //call to timer
+    //TODO: Create a timer that starts at 10 minutes and counts down in real time.
+    //TODO: Create a one minute time buff
+    //TODO: create a 3 minute timer when blue liquid is removed from refrigerator, turn item Red.
+    //TODO: create a 4 minute timer penalty for the wrong recipe.
+
+    //recipe selector
+    //TODO: randomly select an ENUM index number for the combo and compare final mix to it
+
+    //text input
+    //TODO: Accept user adjective text inputs.
+
+    //player class
+    //TODO look around room, if character defer to character class
+    //TODO Create Backpack (array)
+
+
+    //mansion
+    //TODO:
+    //TODO:Illegal move exception
+    //TODO:Character class
+    //TODO:Player Class
+    //TODO:Room attributes
+    //
+
+    //characters
+    //TODO: Contains Item
+    //TODO: Question enums
+    //TODO: Expected input field
+
+    //object
+    //TODO:
+    //
+
+}
+
+
