67652e0d419d58062846cee5e6a73c857254f600
tupleHunden <rj@rjsmith.me>
2020-07-21 16:58:13 -0400

    Refactored and updated Jay

--- game/src/com/rjmj/capstone/character/Jay.java
+++ game/src/com/rjmj/capstone/character/Jay.java
@@ -3,17 +3,10 @@ package com.rjmj.capstone.character;
 import java.io.IOException;
 import java.util.Scanner;
 
-
 public class Jay implements Character {
 
     @Override
-    public void talk() {
-
-    }
-
-    @Override
-    public String askQuestion() throws IOException {
-        String result = "";
+    public String askTheQuestionAndCollectInput() throws IOException {
         String url_open = "https://www.youtube.com/watch?v=hyctW2abkY4";
         java.awt.Desktop.getDesktop().browse(java.net.URI.create(url_open));
         System.out.println("What band is Jay playing in this video?");
@@ -23,20 +16,19 @@ public class Jay implements Character {
         System.out.println("D. Judas Priest");
 
         Scanner sc = new Scanner(System.in);
-        String answer = sc.next();
-        if (answer.toUpperCase().equals("C")) {
+        return sc.next();
+    }
+
+    @Override
+    public String processQuestionAnswer(String questionAnswer) {
+        String result = "";
+        if (questionAnswer.toUpperCase().equals("C")) {
             System.out.println("Correct");
             result = "Red Liquid";
         } else {
             System.out.println("Incorrect, please try again.");
-            askQuestion();
+            processQuestionAnswer(questionAnswer);
         }
         return result;
-}
-
-
-    @Override
-    public void answerQuestion() {
-
     }
 }
