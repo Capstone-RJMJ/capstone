9bc122c71fbe528fff7a258b131d0481b87d2c7b
tupleHunden <rj@rjsmith.me>
2020-07-21 17:17:41 -0400

    Refactored Zach

--- game/src/com/rjmj/capstone/character/Zach.java
+++ game/src/com/rjmj/capstone/character/Zach.java
@@ -3,15 +3,10 @@ package com.rjmj.capstone.character;
 import java.util.Scanner;
 
 public class Zach implements Character{
+    private String questionAnswer;
 
     @Override
-    public void talk() {
-
-    }
-
-    @Override
-    public String askQuestion() {
-        String result = "";
+    public void askTheQuestionAndCollectInput() {
         System.out.println("What did the crew name the Dragon DEMO 2 capsule?");
         System.out.println("A. Endeavor");
         System.out.println("B. Atlantis");
@@ -19,20 +14,27 @@ public class Zach implements Character{
         System.out.println("D. Enterprise");
 
         Scanner sc = new Scanner(System.in);
-        String answer = sc.next();
-        System.out.println(answer);
-        if (answer.toUpperCase().equals("A")) {
+        processQuestionAnswer(sc.next());
+    }
+
+    @Override
+    public String processQuestionAnswer(String questionAnswer) {
+        String result = "";
+        if (getQuestionAnswer().toUpperCase().equals("A")) {
             System.out.println("Correct");
             result = "Recipe";
         } else {
             System.out.println("Incorrect, please try again.");
-            askQuestion();
+            askTheQuestionAndCollectInput();
         }
         return result;
     }
 
-    @Override
-    public void answerQuestion() {
+    public String getQuestionAnswer() {
+        return questionAnswer;
+    }
 
+    public void setQuestionAnswer(String questionAnswer) {
+        this.questionAnswer = questionAnswer;
     }
 }
