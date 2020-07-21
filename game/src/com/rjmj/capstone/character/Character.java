a18eeec4bc3147b58bc34786936d72cfce2972bf
tupleHunden <rj@rjsmith.me>
2020-07-21 16:58:38 -0400

    Added the IO Exception back to Character Interface for Jay

--- game/src/com/rjmj/capstone/character/Character.java
+++ game/src/com/rjmj/capstone/character/Character.java
@@ -1,9 +1,11 @@
 package com.rjmj.capstone.character;
 
+import java.io.IOException;
+
 public interface Character {
     // This will warn you that the return value is never used, but it is in the characters.
     // Public and Abstract are redundant for an interface, everything inside it is Public Abstract already.
-    String askTheQuestionAndCollectInput();
+    String askTheQuestionAndCollectInput() throws IOException;
 
     String processQuestionAnswer(String questionAnswer);
 }
