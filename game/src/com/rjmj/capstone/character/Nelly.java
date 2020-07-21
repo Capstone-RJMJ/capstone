20979487a9119f0fddd0e7bdf76b48b1e515f0d4
tupleHunden <rj@rjsmith.me>
2020-07-21 17:05:40 -0400

    Refactored Nelly

--- game/src/com/rjmj/capstone/character/Nelly.java
+++ game/src/com/rjmj/capstone/character/Nelly.java
@@ -3,25 +3,26 @@ package com.rjmj.capstone.character;
 import java.util.Scanner;
 
 public class Nelly implements Character {
+    private String questionAnswer;
 
     @Override
-    public void talk() {
+    public void askTheQuestionAndCollectInput() {
         System.out.println("Welcome to the Apprenticeship!  Your journey has been hard, but I will help you");
         System.out.println("You have been intentionally injected with CoVID.  Luckily the vaccine ingredients");
         System.out.println("are located in this Mansion.  If you fail to properly mix and administer the vaccine");
         System.out.println("within 10 minutes you will die.");
         System.out.println("To move around, you simply will type 'Up' 'Back' 'Left' and 'Right' as applicable.");
         System.out.println("To get you started here is a Syringe.  You will need to find the remaining items");
+        System.out.println("Type Start if you are ready to get started.");
+        Scanner sc = new Scanner(System.in);
+        setQuestionAnswer(sc.next());
     }
 
     @Override
-    public String askQuestion() {
+    public String processQuestionAnswer(String questionAnswer) {
         String result = "";
-        System.out.println("Type Start if you are ready to get started.");
-        Scanner sc = new Scanner(System.in);
-        String answer = sc.next();
-        if (answer.toUpperCase().equals("START")) {
-            talk();
+        if (getQuestionAnswer().toUpperCase().equals("START")) {
+            askTheQuestionAndCollectInput();
             System.out.println("Good Luck");
             result = "Syringe";
         } else {
@@ -31,8 +32,11 @@ public class Nelly implements Character {
         return result;
     }
 
-    @Override
-    public void answerQuestion() {
+    public String getQuestionAnswer() {
+        return questionAnswer;
+    }
 
+    public void setQuestionAnswer(String questionAnswer) {
+        this.questionAnswer = questionAnswer;
     }
 }
