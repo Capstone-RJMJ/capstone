eed678c5a37d6d684ae9dff1a76268c6c8eddf8d
tupleHunden <rj@rjsmith.me>
2020-07-22 14:32:10 -0400

    Modified Nelly to have only Nelly-specific text and not general intro text.

--- game/src/com/rjmj/capstone/character/Nelly.java
+++ game/src/com/rjmj/capstone/character/Nelly.java
@@ -7,12 +7,8 @@ public class Nelly implements Character {
 
     @Override
     public String askTheQuestionAndCollectInput() {
-        System.out.println("Welcome to the Apprenticeship!  Your journey has been hard, but I will help you");
-        System.out.println("You have been intentionally injected with CoVID.  Luckily the vaccine ingredients");
-        System.out.println("are located in this Mansion.  If you fail to properly mix and administer the vaccine");
-        System.out.println("within 10 minutes you will die.");
-        System.out.println("To move around, you simply will type 'Up' 'Back' 'Left' and 'Right' as applicable.");
-        System.out.println("To get you started here is a Syringe.  You will need to find the remaining items");
+        System.out.println("To get you started here is a Syringe.  You will need to find the remaining items.");
+        System.out.println("Once you have all of the items required, the mix command will become available for you.");
         System.out.println("Type Start if you are ready to get started.");
         Scanner sc = new Scanner(System.in);
         setQuestionAnswer(sc.next());
