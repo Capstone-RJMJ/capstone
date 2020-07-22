358bc7bc50ec718c8b042b9d1425e72a8f790711
tupleHunden <rj@rjsmith.me>
2020-07-22 12:28:51 -0400

    Updated Client to reflect the new playGame method

--- game/src/com/rjmj/capstone/Client.java
+++ game/src/com/rjmj/capstone/Client.java
@@ -1,14 +1,15 @@
 package com.rjmj.capstone;
 
 import com.rjmj.capstone.player.Player;
+import com.rjmj.capstone.room.GameTestArt;
 
 import java.io.IOException;
 
 public class Client {
     public static void main(String[] args) throws IOException {
         Player player = new Player();
+        GameTestArt gameTestArt = new GameTestArt();
 
-        player.collectPlayerName();
-        player.availableActions();
+        player.playGame();
     }
 }
