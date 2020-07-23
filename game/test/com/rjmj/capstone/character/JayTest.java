ecc022dca680c59d6a4751e7d3c43ad2eabcb8bd
tupleHunden <rj@rjsmith.me>
2020-07-23 12:43:53 -0400

    Unit Tests for Jay

--- game/test/com/rjmj/capstone/character/JayTest.java
+++ game/test/com/rjmj/capstone/character/JayTest.java
@@ -9,28 +9,73 @@ import static org.junit.Assert.*;
 public class JayTest {
 
     @Test
-    public void answerWithCNegative() throws IOException, InterruptedException {
-        Jay zach = new Jay();
-        assertEquals("Red Liquid", zach.processQuestionAnswer("C"));
+    public void processQuestionAnswerPositiveC() throws IOException, InterruptedException {
+        String input = "C";
+        Jay jay = new Jay();
+        assertEquals("Red Liquid", jay.processQuestionAnswer(input));
     }
 
-    // These three tests pass, during the test run they will go straight into Scanner In with the question asked again.
-    // This is the behavior that is designed, causing a "passing" test.
+    // You will have to comment out the askTheQuestionAndCollectInput() method inside Jay for these tests to pass.
+    // Otherwise, the test will go right into scanner and break.
     @Test
-    public void answerWithBNegative() throws IOException, InterruptedException {
-        Jay zach = new Jay();
-        assertEquals("\"Incorrect, please try again.\"", zach.processQuestionAnswer("B"));
-    }
-
-    @Test
-    public void answerWithANegative() throws IOException, InterruptedException {
-        Jay zach = new Jay();
-        assertEquals("\"Incorrect, please try again.\"", zach.processQuestionAnswer("A"));
+    public void processQuestionAnswerNegativeA() throws IOException, InterruptedException {
+        String input = "A";
+        Jay jay = new Jay();
+        assertNotEquals("Red Liquid", jay.processQuestionAnswer(input));
     }
 
     @Test
-    public void answerWithDNegative() throws IOException, InterruptedException {
-        Jay zach = new Jay();
-        assertEquals("\"Incorrect, please try again.\"", zach.processQuestionAnswer("D"));
+    public void processQuestionAnswerNegativeB() throws IOException, InterruptedException {
+        String input = "B";
+        Jay jay = new Jay();
+        assertNotEquals("Red Liquid", jay.processQuestionAnswer(input));
+    }
+
+    @Test
+    public void processQuestionAnswerNegativeD() throws IOException, InterruptedException {
+        String input = "D";
+        Jay jay = new Jay();
+        assertNotEquals("Red Liquid", jay.processQuestionAnswer(input));
+    }
+
+    // Simple Getter/Setter Tests
+    @Test
+    public void setQuestionAnswerAPositive() {
+        Jay jay = new Jay();
+        jay.setQuestionAnswer("A");
+        assertEquals("A", jay.getQuestionAnswer());
+    }
+
+    @Test
+    public void setQuestionAnswerBPositive() {
+        Jay jay = new Jay();
+        jay.setQuestionAnswer("B");
+        assertEquals("B", jay.getQuestionAnswer());
+    }
+
+    @Test
+    public void setQuestionAnswerCPositive() {
+        Jay jay = new Jay();
+        jay.setQuestionAnswer("C");
+        assertEquals("C", jay.getQuestionAnswer());
+    }
+
+    @Test
+    public void setQuestionAnswerDPositive() {
+        Jay jay = new Jay();
+        jay.setQuestionAnswer("D");
+        assertEquals("D", jay.getQuestionAnswer());
+    }
+
+    @Test
+    public void getItemPositive() {
+        Jay jay = new Jay();
+        assertEquals("Red Liquid", jay.getItem());
+    }
+
+    @Test
+    public void getItemNegative() {
+        Jay jay = new Jay();
+        assertNotEquals("Blue Liquid", jay.getItem());
     }
 }
