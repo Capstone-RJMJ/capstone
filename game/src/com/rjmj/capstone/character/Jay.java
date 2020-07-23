32bfdfd5d66a11ecb782fcfdc0de0abc0962b59f
tupleHunden <rj@rjsmith.me>
2020-07-23 09:46:13 -0400

    Added talking delay to Jay

--- game/src/com/rjmj/capstone/character/Jay.java
+++ game/src/com/rjmj/capstone/character/Jay.java
@@ -7,14 +7,24 @@ public class Jay implements Character {
     private String questionAnswer;
 
     @Override
-    public String askTheQuestionAndCollectInput() throws IOException {
+    public String askTheQuestionAndCollectInput() throws IOException, InterruptedException {
+        String[] jayInput = {
+                ANSI_CYAN,
+                "What band is Jay playing in this video?",
+                "A. Aerosmith",
+                "B. Daft Punk",
+                "C. Queen",
+                "D. Judas Priest",
+                ANSI_RESET
+        };
+
         String url_open = "https://www.youtube.com/watch?v=hyctW2abkY4";
         java.awt.Desktop.getDesktop().browse(java.net.URI.create(url_open));
-        System.out.println("What band is Jay playing in this video?");
-        System.out.println("A. Aerosmith");
-        System.out.println("B. Daft Punk");
-        System.out.println("C. Queen");
-        System.out.println("D. Judas Priest");
+
+        for (String jay : jayInput) {
+            Thread.sleep(1000);
+            System.out.println(jay);
+        }
 
         Scanner sc = new Scanner(System.in);
         setQuestionAnswer(sc.next());
@@ -22,7 +32,7 @@ public class Jay implements Character {
     }
 
     @Override
-    public String processQuestionAnswer(String questionAnswer) throws IOException {
+    public String processQuestionAnswer(String questionAnswer) throws IOException, InterruptedException {
         String result = "";
         if (questionAnswer.toUpperCase().equals("C")) {
             System.out.println("Correct");
