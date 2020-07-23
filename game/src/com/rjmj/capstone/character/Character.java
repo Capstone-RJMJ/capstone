709a8d3db049c221d093ef9f04f495cd9e08b454
tupleHunden <rj@rjsmith.me>
2020-07-23 09:44:10 -0400

    Added color statics to the Character interface to be used across all characters.

--- game/src/com/rjmj/capstone/character/Character.java
+++ game/src/com/rjmj/capstone/character/Character.java
@@ -3,8 +3,11 @@ package com.rjmj.capstone.character;
 import java.io.IOException;
 
 public interface Character {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+
     // This will warn you that the return value is never used, but it is in the characters.
-    String askTheQuestionAndCollectInput() throws IOException;
+    String askTheQuestionAndCollectInput() throws IOException, InterruptedException;
 
     String processQuestionAnswer(String questionAnswer) throws IOException;
 }
