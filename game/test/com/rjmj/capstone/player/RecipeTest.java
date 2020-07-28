29c363896822f80189cfcdd7ef49dcd5a7d7d81f
michael lennerblom <mlennerblom@gmail.comgit config --listgit config --global user.name Michael Lennerblomgit config --global user.email mlennerblom@gmail.comgit config --listgit config --global user.name Michael>
2020-07-28 14:38:22 -0700

    added another recipe test

--- game/test/com/rjmj/capstone/player/RecipeTest.java
+++ game/test/com/rjmj/capstone/player/RecipeTest.java
@@ -19,20 +19,29 @@ public class RecipeTest {
         playerMix.add(Color.GREEN);
         playerMix.add(Color.RED);
         playerMix.add(Color.BLUE);
-
+    }
+    @Test
+    public void isRecipeMatchPositive() {
         recipe.colorMix.add(Color.GREEN);
         recipe.colorMix.add(Color.RED);
         recipe.colorMix.add(Color.BLUE);
-    }
-
-    @Test
-    public void isRecipeMatchPositive() {
         boolean actual = recipe.isRecipeMatch(playerMix,cd);
         assertEquals(true,actual);
     }
     @Test
     public void isRecipeMatchNegative() {
+        recipe.colorMix.add(Color.GREEN);
+        recipe.colorMix.add(Color.RED);
+        recipe.colorMix.add(Color.RED);
         boolean actual = recipe.isRecipeMatch(playerMix,cd);
         assertNotEquals(true,actual);
+    }
+    @Test
+    public void mixRandomRecipeContainsOneOfEachColorPositive() {
+        recipe.mixRandomRecipe();
+        assertEquals(3,recipe.getColorMix().size());
+        assertTrue(recipe.getColorMix().contains(Color.RED));
+        assertTrue(recipe.getColorMix().contains(Color.BLUE));
+        assertTrue(recipe.getColorMix().contains(Color.GREEN));
     }
 }
